3c4bec12b244143cc9b6fdb39da638e356858263
Mathew Elston <elstonmathew@yahoo.com>
2023-07-27 10:06:51 -0700

    Spinner during fetch genre

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,13 @@
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image, HStack } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Heading,
+  Image,
+  HStack,
+  Box,
+  Center,
+} from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/img-url";
@@ -11,14 +19,17 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card>
-      <Image src={getCroppedImageUrl(game.background_image)} />
+      <Image borderRadius={5} src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
-          <CriticScore score={game.metacritic} />
+        </HStack>
+        <HStack justifyContent={"center"}>
+
+            <CriticScore score={game.metacritic} />
         </HStack>
       </CardBody>
     </Card>
